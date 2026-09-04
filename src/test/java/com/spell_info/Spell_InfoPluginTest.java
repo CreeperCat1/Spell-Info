d7b2f3cf30de6eeb7bc3bc64283eba56f739d233
CreeperCat1 <noahcguttmann@gmail.com>
2026-09-03 20:09:32 -0700

    start of refactoring the example plugin -renames all example parts of the example plugin -sets up some of the initial plugin information

--- src/test/java/com/spell_info/Spell_InfoPluginTest.java
+++ src/test/java/com/spell_info/Spell_InfoPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.spell_info;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class Spell_InfoPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(Spell_InfoPlugin.class);
 		RuneLite.main(args);
 	}
 }
